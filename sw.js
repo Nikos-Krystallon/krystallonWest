16a27307f06730374dea5c8b3bbd84a55b2eda27
Nikos-Krystallon <krystallonvillas@gmail.com>
2026-04-27 00:01:24 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
-const CACHE = 'krystallon-1776893433937';
-const SHELL = ['./index.html', './manifest.json', './logo.png'];
-self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL))); self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
-self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(c => c || fetch(e.request)).catch(() => caches.match('./index.html'))); });
-self.addEventListener('message', e => { if(e.data && e.data.action === 'skipWaiting') self.skipWaiting(); });
+const CACHE = 'krystallon-1777237221541';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => {
+  if(e.request.mode === 'navigate'){
+    e.respondWith(fetch(e.request).catch(() => caches.match('./index.html')));
+  } else {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  }
+});
